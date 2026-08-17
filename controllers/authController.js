cd9be1f1fc12e2e439a9575ed4bb03617862b027
Om Dhakal <omdhakal1019@gmail.com>
2026-08-17 17:29:33 +0545

    login with jwt response

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// SignUp controller
 
 export const signupUser = async (req, res) => {
   try {
@@ -33,3 +36,47 @@ export const signupUser = async (req, res) => {
 };
 
 export default signupUser;
+
+// Login Controller
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // First Checking If The User Exists Or Not
+
+    const userExist = await User.findOne({ email });
+
+    if (!userExist) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    // Now Checking If The Password Is Correct
+
+    const check = await bcrypt.compare(password, userExist.password);
+
+    if (!check) {
+      return res.status(400).json({ message: "Invalid Credidentials" });
+    }
+
+    // Assigning Jwt token
+
+    const token = jwt.sign({ id: userExist._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    // Sucessfull Response
+
+    res.json({
+      message: "Login successfull",
+      token,
+      user: {
+        id: userExist._id,
+        name: userExist.name,
+        email: userExist.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Can't find the user" });
+  }
+};
